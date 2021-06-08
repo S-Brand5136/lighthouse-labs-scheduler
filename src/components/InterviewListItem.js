85a7fe8567d84ec8ed13371a8552cb9f69711bb6
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-06-08 15:21:06 -0600

    add props to interviewListItem

--- src/components/InterviewListItem.js
+++ src/components/InterviewListItem.js
@@ -1,14 +1,21 @@
 import React from 'react'
+import classNames from 'classnames';
 
-const InterviewListItem = () => {
+import "components/InterviewListItem.scss";
+
+const InterviewListItem = (props) => {
+  const liClass = classNames('interviewers__item',
+    {'interviewers__item--selected': props.selected}
+  )
+
   return (
-    <li className="interviewers__item">
+    <li className={liClass} onClick={() => props.setInterviewer(props.id)}>
       <img
         className="interviewers__item-image"
-        src="https://i.imgur.com/LpaY82x.png"
+        src={props.avatar}
         alt="Sylvia Palmer"
       />
-      Sylvia Palmer
+      {props.selected ? props.name : ''}
     </li>
   )
 }
