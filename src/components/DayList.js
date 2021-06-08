83c79364746999fc5df2d569a1b0d42258fdeb86
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-06-07 18:55:55 -0600

    add button component, dayListItem component, and file for dayList component

---
+++ src/components/DayList.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+    // Props
+    // days:Array a list of day objects (each object includes an id, name, and spots)
+    // day:String the currently selected day
+    // setDay:Function accepts the name of the day eg. "Monday", "Tuesday"
+
+
+const DayList = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default DayList
---
+++ src/components/DayListItem.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import classNames from "classnames";
+
+import "components/DayListItem.scss";
+
+const DayListItem = (props) => {
+  const liClass = classNames('day-list__item', {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  })
+
+  const formatSpots = () => {
+    if(props.spots <= 0) {
+      return <h3 className='text--light'>no spots remaining</h3>
+    }
+
+    if(props.spots === 1) {
+      return <h3 className='text--light'>{props.spots} spot remaining</h3>
+    }
+    return <h3 className='text--light'>{props.spots} spots remaining</h3>
+  }
+  
+
+  return (
+    <li className={liClass} onClick={() => props.setDay(props.name)}>
+      <h2 className='text--regular'>{props.name}</h2>
+      {formatSpots()}
+    </li>
+  )
+}
+
+export default DayListItem
