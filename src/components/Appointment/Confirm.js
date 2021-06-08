6bba3081398a468fd001bd7a4f4b61831b381e21
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-06-08 17:12:18 -0600

    refactor Confirm component to use props

--- src/components/Appointment/Confirm.js
+++ src/components/Appointment/Confirm.js
@@ -2,13 +2,17 @@ import React from 'react';
 
 import Button from '../Button';
 
-const Confirm = () => {
+const Confirm = (props) => {
   return (
     <main className='appointment__card appointment__card--confirm'>
-      <h1 className='text--semi-bold'>Delete the appointment?</h1>
+      <h1 className='text--semi-bold'>{props.message}</h1>
       <section className='appointment__actions'>
-        <Button danger>Cancel</Button>
-        <Button danger>Confirm</Button>
+        <Button onClick={props.onCancel} danger>
+          Cancel
+        </Button>
+        <Button onClick={props.onConfirm} danger>
+          Confirm
+        </Button>
       </section>
     </main>
   );
