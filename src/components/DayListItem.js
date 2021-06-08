efdfa6412babc08efc95cb46689271c98521f10d
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-06-07 19:10:11 -0600

    Add comments, remove whitespace in DayListItem.js

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -9,18 +9,17 @@ const DayListItem = (props) => {
     "day-list__item--full": props.spots === 0
   })
 
+// Formats message for num of appointment spots left in a day
   const formatSpots = () => {
     if(props.spots <= 0) {
       return <h3 className='text--light'>no spots remaining</h3>
     }
-
     if(props.spots === 1) {
       return <h3 className='text--light'>{props.spots} spot remaining</h3>
     }
     return <h3 className='text--light'>{props.spots} spots remaining</h3>
   }
   
-
   return (
     <li className={liClass} onClick={() => props.setDay(props.name)}>
       <h2 className='text--regular'>{props.name}</h2>
