2d671848aa84b203858c2dd74f8d3b3d9e397c23
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-06-08 15:39:16 -0600

    Add helper function to render interviewerItems

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,10 +1,26 @@
 import React from 'react';
 
-const InterviewerList = () => {
+import InterviewListItem from './InterviewListItem';
+import 'components/InterviewerList.scss';
+
+const InterviewerList = (props) => {
+  // Creates an array of InterviewListItems to render
+  const interviewerList = props.interviewers.map((item) => {
+    return (
+      <InterviewListItem
+        key={item.id}
+        setInterviewer={props.setInterviewer}
+        selected={props.interviewer === item.id}
+        name={item.name}
+        avatar={item.avatar}
+      />
+    );
+  });
+
   return (
     <section className='interviewers'>
       <h4 className='interviewers__header text--light'>Interviewer</h4>
-      <ul className='interviewers__list'></ul>
+      <ul className='interviewers__list'>{interviewerList}</ul>
     </section>
   );
 };
