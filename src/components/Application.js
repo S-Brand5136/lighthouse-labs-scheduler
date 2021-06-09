6254508a19323d85e99a0dc1b5784ebfa2ebbc32
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-06-08 18:53:32 -0600

    Add dumby data for appointments at the top of application.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -22,6 +22,45 @@ const days = [
   },
 ];
 
+const appointments = [
+  {
+    id: 1,
+    time: '12pm',
+  },
+  {
+    id: 2,
+    time: '1pm',
+    interview: {
+      student: 'Lydia Miller-Jones',
+      interviewer: {
+        id: 1,
+        name: 'Sylvia Palmer',
+        avatar: 'https://i.imgur.com/LpaY82x.png',
+      },
+    },
+  },
+  {
+    id: 3,
+    time: '2pm',
+  },
+  {
+    id: 4,
+    time: '3pm',
+    interview: {
+      student: 'Cad-Bane',
+      interviewer: {
+        id: 1,
+        name: 'Sven Jones',
+        avatar: 'https://i.imgur.com/twYrpay.jpg',
+      },
+    },
+  },
+  {
+    id: 5,
+    time: '4pm',
+  },
+];
+
 export default function Application(props) {
   const [day, setDay] = useState('Monday');
 
