8e25cfc80cc95e4c097eff43c9ac33afd138516c
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-06-09 15:22:56 -0600

    import axios and useEffect hook, remove days array at top of file

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,27 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 import DayList from './DayList';
 import Appointment from 'components/Appointment';
 
 import 'components/Application.scss';
-
-const days = [
-  {
-    id: 1,
-    name: 'Monday',
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: 'Tuesday',
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: 'Wednesday',
-    spots: 0,
-  },
-];
 
 const appointments = [
   {
