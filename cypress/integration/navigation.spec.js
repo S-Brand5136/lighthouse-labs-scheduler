cf3f35b71b1c9dd72caa98fff51f1756ade8125d
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-06-16 14:04:44 -0600

    refactor common code into a beforeEach function

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,11 +1,11 @@
 describe('Navigation', () => {
-  it('should visit root', () => {
+  beforeEach(() => {
     cy.visit('/');
   });
 
-  it('should navigate to Tuesday', () => {
-    cy.visit('/');
+  it('should visit root', () => {});
 
+  it('should navigate to Tuesday', () => {
     cy.contains('[data-testid=day]', 'Tuesday')
       .click()
       .should('have.class', 'day-list__item--selected');
