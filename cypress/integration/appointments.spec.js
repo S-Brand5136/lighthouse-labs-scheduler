e7da6941f0680d2650d79dbcdf04d81d5560b125
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-06-16 14:04:25 -0600

    Add beforeEach, and first test code to check booking interviews

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,3 +1,22 @@
 describe('Appointments', () => {
-  it('should book an interview', () => {});
+  beforeEach(() => {
+    cy.request('GET', 'http://localhost:8000/api/debug/reset');
+
+    cy.visit('/');
+
+    cy.contains('Monday');
+  });
+
+  it('should book an interview', () => {
+    cy.get('[alt="Add"]').first().click();
+
+    cy.get('[data-testid=student-name-input]').type('Lydia Miller-Jones');
+
+    cy.get('[alt="Sylvia Palmer"]').click();
+
+    cy.contains('Save').click();
+
+    cy.contains('.appointment__card--show', 'Lydia Miller-Jones');
+    cy.contains('.appointment__card--show', 'Sylvia Palmer');
+  });
 });
