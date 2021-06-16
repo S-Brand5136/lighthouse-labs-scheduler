e718ff59f83e3272d75583918138c347e157a5f8
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-06-15 18:19:31 -0600

    Create reducers dir, add reducer function and action consts from useApplicationData hook

---
+++ src/reducers/application.js
@@ -0,0 +1,33 @@
+import { useReducer } from 'react';
+
+const SET_DAY = 'SET_DAY';
+const SET_APPLICATION_DATA = 'SET_APPLICATION_DATA';
+const SET_INTERVIEW = 'SET_INTERVIEW';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case SET_DAY: {
+      return { ...state, day: action.value.day };
+    }
+    case SET_APPLICATION_DATA: {
+      return {
+        ...state,
+        days: action.value.days,
+        appointments: action.value.appointments,
+        interviewers: action.value.interviewers,
+      };
+    }
+    case SET_INTERVIEW: {
+      return {
+        ...state,
+        appointments: action.value.appointments,
+        days: action.value.days,
+      };
+    }
+    default: {
+      throw new Error(
+        `Tried to reduce with unsupported action type: ${action.type}`
+      );
+    }
+  }
+};
